315d96b02865f5209a869add6de0607ea140cd02
Rickos99 <Rickos99@users.noreply.github.com>
2021-11-30 16:33:36 +0100

    Added buffer to household model

--- src/models/household.ts
+++ src/models/household.ts
@@ -7,16 +7,28 @@ export default class Household {
     public readonly owner: string;
 
     // House properties
+    /** Total indoor area of household. Unit square meters [m²] */
     public readonly area: number;
+
     public readonly location: Location;
 
     // Consumption properties
+    /** Consumption during low peak hours  */
     public readonly baseConsumption: number;
     public readonly heatingEfficiency: number;
 
+    // Buffer
+    /** Energy in buffer. Unit Joule [J] */
+    public energyInBuffer: number; // J
+
     // Production properties
+    /** Number of active windturbines */
     public readonly numberOfActiveWindturbines: number;
+
+    /** Maximum production of a windturbine. Unit Watt [W] */
     public readonly maximumWindturbineProduction: number;
+
+    /** Cut-out windspeed during production. Unit meter per second [m/s] */
     public readonly productionCutoutWindspeed: number;
 
     private consumptionNoiseDist: distribution;
@@ -31,6 +43,29 @@ export default class Household {
         this.baseConsumption = 500;
         this.consumptionHighSpikeGenerator = new EffectSpikeGenerator(1100, 3500, 15, 60);
         this.consumptionNoiseDist = new TruncatedNormalDistribution(-100, 200);
+    }
+
+    /**
+     * Add energy to buffer
+     * @param energy Energy to add to buffer. Unit Joule [J]
+     * @returns Total energy in buffer after more energy was added.
+     */
+    public AddEnergyToBuffer(energy: number): number {
+        this.energyInBuffer += energy;
+        return this.energyInBuffer;
+    }
+
+    /**
+     * Take energy stored in buffer. If more energy is taken than avaliable in
+     * the buffer, no energy is taken and an error is thrown.
+     * @param energyToTake Energy to take from buffer
+     * @returns Energy left in buffer after taking energy from buffer
+     */
+    public TakeEnergyInBuffer(energyToTake: number): number {
+        if (this.energyInBuffer < energyToTake) throw new Error("Exceeded avaliable energy in buffer");
+
+        this.energyInBuffer -= energyToTake;
+        return this.energyInBuffer;
     }
 
     /**

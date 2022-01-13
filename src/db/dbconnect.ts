cb9a320974cfa1507199fba61a63e45ae0464fee
Rickos99 <Rickos99@users.noreply.github.com>
2022-01-13 19:08:35 +0100

    Fixed connection error when credentials are specified

--- src/db/dbconnect.ts
+++ src/db/dbconnect.ts
@@ -5,15 +5,19 @@ const DB_NAME = process.env.MONGODB_DATABASE;
 const DB_USERNAME = process.env.MONGODB_USERNAME;
 const DB_PASSWORD = process.env.MONGODB_PASSWORD;
 
-const DB_CREDENTIALS =
-    DB_USERNAME !== undefined && DB_PASSWORD !== undefined
-        ? `${encodeURIComponent(DB_NAME)}:${encodeURIComponent(DB_PASSWORD)}@`
-        : "";
-const DB_CONNECTION_STRING = `mongodb:${DB_CREDENTIALS}//${DB_HOST}/${DB_NAME}`;
+const DB_CREDENTIALS = DB_USERNAME !== undefined && DB_PASSWORD !== undefined;
+const DB_CONNECTION_STRING = `mongodb://${DB_HOST}/${DB_NAME}`;
 
-console.log(`DB_CONNECTION_STRING: ${DB_CONNECTION_STRING}`);
+const options: any = {};
+if (DB_CREDENTIALS) {
+    options.authSource = "admin";
+    options.user = DB_USERNAME;
+    options.pass = DB_PASSWORD;
+}
 
-mongoose.connect(DB_CONNECTION_STRING, {}, err => {
+mongoose.connect(DB_CONNECTION_STRING, options, err => {
+    console.log(`DB_CONNECTION_STRING: ${DB_CONNECTION_STRING}`);
+
     if (err) throw err;
     console.log(`Connected to database 'test' at ${DB_HOST}`);
 });
